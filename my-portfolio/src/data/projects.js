c2d0cae4cffdda50df711f14b8189f3978aa45b7
Arnie <yenyuripadua@gmail.com>
2026-09-17 13:56:53 +0800

    For develop

--- my-portfolio/src/data/projects.js
+++ my-portfolio/src/data/projects.js
@@ -27,5 +27,37 @@ export const projects = [
             "A two-person interactive portfolio presented as a retro pixel-art character selection experience.",
         tech: ["React", "CSS", "JavaScript"],
         status: "IN DEVELOPMENT"
-    }
+    },
+
+    {
+        id: 4,
+        title: "Student Library Managemrnt System",
+        type: "PYTHON APPLICATION",
+        description:
+            "An application to help them manage a library (Record of Book and Book borrowed)",
+        tech: ["Python"],
+        status: "COMPLETED"
+    },
+
+    {
+        id: 5,
+        title: "STUDENT TIME MANAGEMENT SYSTEM",
+        type: "C++ APPLICATION",
+        description:
+            "An application for student to facilitate their schedule and manage their time",
+         tech: ["C++"],
+        status: "COMPLETED"
+    },
+
+    {
+        id: 6,
+        title: "TYPORIA THE KEY MASTERS CODE",
+        type: "JAVA APPLICATION",
+        description:
+            "A game that develop using java, a typing game with a code tracing, code analyze",
+        tech: ["Java"],
+        status: "COMPLETED"
+    },
+
+
 ];
